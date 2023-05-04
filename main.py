ba1b47bfb557de20215fa9e31fe1b986f4a1fcdd
yamayafumiteru <fumiteru322@gmail.com>
2023-05-04 12:41:11 +0900

    create class wandbtrainer

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 import wandb
 import random
 
-# start a new wandb run to track this script
-wandb.init(
-    # set the wandb project where this run will be logged
-    project="my-awesome-project",
-    # track hyperparameters and run metadata
-    config={
-        "learning_rate": 0.02,
-        "architecture": "CNN",
-        "dataset": "CIFAR-100",
-        "epochs": 10,
-    },
-)
 
-# simulate training
-epochs = 10
-offset = random.random() / 5
-for epoch in range(2, epochs):
-    acc = 1 - 2**-epoch - random.random() / epoch - offset
-    loss = 2**-epoch + random.random() / epoch + offset
+class WandBTrainer:
+    def __init__(self):
+        self.wandb_project = "my-awesome-project"
+        self.learning_rate = 0.02
+        self.architecture = "CNN"
+        self.dataset = "CIFAR-100"
+        self.epochs = 10
 
-    # log metrics to wandb
-    wandb.log({"acc": acc, "loss": loss})
+        # start a new wandb run to track this script
+        wandb.init(
+            # set the wandb project where this run will be logged
+            project=self.wandb_project,
+            # track hyperparameters and run metadata
+            config={
+                "learning_rate": self.learning_rate,
+                "architecture": self.architecture,
+                "dataset": self.dataset,
+                "epochs": self.epochs,
+            },
+        )
 
-accuracy = 0.9
-# 通知の送信
-wandb.alert(title="WandBからの通知", text=f"今の正解率は {accuracy} です。")
+    def train(self):
+        # simulate training
+        epochs = self.epochs
+        offset = random.random() / 5
+        for epoch in range(2, epochs):
+            acc = 1 - 2**-epoch - random.random() / epoch - offset
+            loss = 2**-epoch + random.random() / epoch + offset
 
-wandb.finish()
+            # log metrics to wandb
+            wandb.log({"acc": acc, "loss": loss})
+
+        accuracy = 0.9
+        # 通知の送信
+        wandb.alert(title="WandBからの通知", text=f"今の正解率は {accuracy} です。")
+
+        wandb.finish()
